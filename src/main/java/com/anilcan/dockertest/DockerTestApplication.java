37841a41ad4e0d0f9075461d7a3dc9e29c5e58d5
Anil Can OZGOK <ozgok1903@gmail.com>
2022-12-18 00:32:03 +0300

    initial commit

---
+++ src/main/java/com/anilcan/dockertest/DockerTestApplication.java
@@ -0,0 +1,22 @@
+package com.anilcan.dockertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerTestApplication {
+
+    @RequestMapping("/")
+    public String home(){
+        return "DockerTest";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerTestApplication.class, args);
+    }
+
+}
